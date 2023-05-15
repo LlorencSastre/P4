43e91018515327e2bfa9b5895e2d32b3a5b00e71
Llorenç Sastre Galmes <sastrellorenc@gmail.com>
2023-05-15 08:30:33 +0200

    Abans de classe 15-05

--- src/pav/gmm.cpp
+++ src/pav/gmm.cpp
@@ -112,6 +112,7 @@ namespace upc
 
 		for (n=0; n<data.nrow(); ++n) {
 			/// \TODO Compute the logprob of a single frame of the input data; you can use gmm_logprob() above.
+			lprob += gmm_logprob(data[n]);
 		}
 		return lprob/data.nrow();
 	}
@@ -213,9 +214,15 @@ namespace upc
 			//
 			// Update old_prob, new_prob and inc_prob in order to stop the loop if logprob does not
 			// increase more than inc_threshold.
-			this->em_expectation(data,weights);
+			new_prob = this->em_expectation(data,weights);
 			this->em_maximization(data,weights);
-			
+
+			inc_prob = new_prob - old_prob;
+			old_prob = new_prob;
+			if(inc_prob < inc_threshold){
+				break;
+			}
+
 			if (verbose & 01)
 				cout << "GMM nmix=" << nmix << "\tite=" << iteration << "\tlog(prob)=" << new_prob << "\tinc=" << inc_prob << endl;
 		}
